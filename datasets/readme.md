9d3948d511425a6ede7fa5fa5a063e9ed4da556a
Dongdong Zhang <104191159+zddfunseek@users.noreply.github.com>
2025-04-07 13:57:56 +0800

    Add files via upload

---
+++ datasets/readme.md
@@ -0,0 +1,1 @@
+Please place the test dataset folder in this directory.
---
+++ script/discontaminate.sh
@@ -0,0 +1,4 @@
+python3 ./utils/remove_leak.py \
+    --target /path/to/input/data.json \
+    --generated /path/to/input/generated_data.json \
+    --output /path/to/output/filtered_data.jsonl
