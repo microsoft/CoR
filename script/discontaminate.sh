8232abb76efdacd2c9889fa0a18d8613739ddc98
yyyhz <92558414+yyyhz@users.noreply.github.com>
2025-04-22 15:52:32 +0800

    Update discontaminate.sh

--- script/discontaminate.sh
+++ script/discontaminate.sh
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
 python3 ./utils/remove_leak.py \
     --target /path/to/input/data.json \
     --generated /path/to/input/generated_data.json \
